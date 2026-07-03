a774e79be0f63e75005d9a4c75b7966025302c3d
BrunZo <ziger.bruno@gmail.com>
2026-07-02 22:06:36 -0300

    org(segtree): organize segment tree code

--- data-structures/setup.py
+++ data-structures/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 
 # Define the extension module
-module = Extension("segtree", sources=["segtree/segtree.c"])
+module = Extension("segtree.segtree", sources=["segtree/segtree.c"])
 
 setup(
     name="Segment Tree",
     version="0.1",
-    description="Python C API package",
+    description="Segment Tree implementation",
     ext_modules=[module],
 )
